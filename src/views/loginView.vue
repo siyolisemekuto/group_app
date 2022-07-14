a5eb9cbea089e92d670e556dca647f767a0eb56f
unknown <aztoorabally7447@gmail.com>
2022-07-14 15:28:46 +0200

    update

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -1,9 +1,9 @@
-<template>
+<!-- <template>
   <div class="hello">
     <form @submit.prevent="login">
       <input type="text" v-model="email" placeholder="email " />
-      <input type="text" v-model="password" placeholder="password" />
-      <button type="submit">submit</button>                             
+      <input type="password" v-model="password" placeholder="password" />
+      <button type="submit">submit</button>
     </form>
     <div v-if="user">Welcome {{ data.username }}</div>
   </div>
@@ -11,20 +11,16 @@
 <script>
 import store from "../store";
 export default {
-  
-  mounted(){
-let user=this.$store.dispatch.login()
+  mounted() {
+    let user = this.$store.dispatch.login();
   },
-  computed: {
-
-
-  },
+  computed: {},
   data() {
     return {
       email: "",
       password: "",
       user: null,
-      username:''
+      username: "",
     };
   },
   methods: {
@@ -32,9 +28,150 @@ let user=this.$store.dispatch.login()
       store.dispatch("login", {
         email: this.email,
         password: this.password,
-        
+      });
+    },
+  },
+};
+</script> -->
+<template>
+  <div id="landing" class="row">
+    <div id="about" class="col">
+      <h1>Inuit</h1>
+      <p class="about-text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+        similique iusto perferendis libero architecto atque accusamus
+        repellendus esse, ut, sapiente reiciendis temporibus voluptas aliquam
+        laboriosam tempora voluptatum voluptatem quibusdam, ex eligendi
+        voluptate totam nihil consequatur illum. Fugiat, obcaecati beatae,
+        repellat recusandae magnam aspernatur facilis praesentium suscipit
+        laudantium fugit cupiditate libero!
+      </p>
+      <div class="container">
+        <h1>Welcome</h1>
+        <form @submit.prevent="login">
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              class="inputs"
+              placeholder="Email"
+              required
+              v-model="email"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              class="inputs"
+              placeholder="Password"
+              required
+              v-model="password"
+            />
+          </div>
+          <button type="submit" class="btn">Submit</button>
+          <div v-if="user">Welcome {{ data.username }}</div>
+        </form>
+      </div>
+    </div>
+    <div class="col">
+      <!-- <img alt="logo" src="../assets/iconrebg.png" /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import store from "../store";
+export default {
+
+  data() {
+    return {
+      email: "",
+      password: "",
+      user: null,
+      username: "",
+    };
+  },
+  methods: {
+    login() {
+      store.dispatch("login", {
+        email: this.email,
+        password: this.password,
       });
     },
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&family=Ubuntu:wght@300&display=swap");
+#landing {
+  display: flex;
+  width: fit-content;
+  background: linear-gradient(0.5turn, #f5f5dc, #d8a23a);
+  height: fit-content;
+}
+.row {
+  display: flex;
+  justify-content: center;
+}
+#about {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  padding: 5%;
+}
+.about-text {
+  width: fit-content;
+  text-align: center;
+}
+img {
+  width: 37rem;
+  height: 50rem;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  background: none;
+  object-fit: cover;
+}
+.container {
+  width: fit-content;
+  height: fit-content;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+}
+.inputs {
+  width: 450px;
+  padding: 10px;
+  margin: 20px;
+  border: none;
+  border-bottom: 1px solid;
+  background: inherit;
+}
+:-ms-input-placeholder::after {
+  display: none;
+}
+.btn {
+  margin-top: 20px;
+  padding: 5px;
+  font-size: large;
+  border-radius: 9px;
+  background: none;
+}
+form {
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+/* @media only screen and (max-width: 600px){ */
+/* #about{
+    display:flex;
+    flex:wrap;
+  }
+} */
+</style>
--- src/views/profileView.vue
+++ src/views/profileView.vue
@@ -13,19 +13,15 @@
       <div class="user_details container">
         <h1>Profile</h1>
         <div id="user-img"><img :src="imgUrl" alt="user" class="img" /></div>
-        <H1
-          ><input type="text" name="user" id="i" :placeholder="UserName"
-        /></H1>
+        <H1><input type="text" v-model="username" name="username" class="i" :placeholder="UserName"/></H1>
+        <H3><input type="email" v-model="email" name="email" class="i" :placeholder="email"/></H3>
+        <H5><input type="password"  v-model="password" name="password" class="i" :placeholder="password"/></H5>
+          <!-- <H2>Bio</H2>  -->
+        <H5><input type="text"  v-model="heading" name="heading" class="i" :placeholder="heading"/></H5>
+        <H5><input type="text"  v-model="blurb" name="blurb" class="i" :placeholder="blurb"/></H5>
+        <p><input type="text"  v-model="nl" name="blurb" class="i" placeholder="replace image"/></p>
         <div class="bio">
-          <H2>Bio</H2>
-          <div class="text">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Reiciendis, saepe totam. Fugit consequatur necessitatibus quis
-              cumque, nostrum et repellat aliquid nobis ducimus ullam
-              repudiandae possimus? Adipisci porro a quas dolorem!
-            </p>
-          </div>
+          
         </div>
       </div>
     </div>
@@ -52,25 +48,51 @@ export default {
   data() {
     return {
       imgUrl:
-        "https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png",
+        "https://cdn.donmai.us/sample/1b/3b/__yoroizuka_mizore_and_kasaki_nozomi_hibike_euphonium_and_1_more_drawn_by_lyy__sample-1b3b61b1a06c197a4c3ab1eba4a70ffc.jpg",
       UserName: "Lihle",
       post_text:
         "Lorem ipsum dolor sit amet consectetur odit distinctio officia laboriosam aliquam at, expedita dolorem earum autem et molestiae necessitatibus architecto aliquid dolore tenetur, voluptatem hic.",
       post_sender: "Username",
       post_sender_img: "https://i.postimg.cc/nrxbv1nL/icon-1.png",
+      password:"****",
+      email:"lihle@gmail.com",
+      heading:"heading",
+      blurb:"blurb",
     };
   },
+  props:{
+
+  }
 };
 </script>
 <style scoped>
 #user-page {
-  background-image: url("https://cdn.shopify.com/s/files/1/1294/9703/products/proud-new-mother-inuit-art-card.gif?v=1625188889");
+    background-image: url("https://cdn.shopify.com/s/files/1/1294/9703/products/proud-new-mother-inuit-art-card.gif?v=1625188889");
+}
+input {
+      background: none;
+      border: none;
+      text-align: center;
+      width: max-content;
+    }
+ 
+.i::after{  
+    font: 1em sans-serif;
+    content: "edit";
+    /* content: "🖉"; */
+}
+
+h1::after{  
+    font: 1em sans-serif;
+    
+    content: "🖉";
 }
 h1,
 h2,
-h3 {
-  color: #f5f5dc;
+h3{
+    color: #f5f5dc;
 }
+
 #user-page {
   height: fit-content;
   background: linear-gradient(0.5turn, #dbdb71, #d8a23a);
@@ -85,15 +107,9 @@ h3 {
   left: -15px;
 }
 .img {
-  width: 200px;
-  border: 2px solid black;
-  border-radius: 50%;
+  width: 40%;
+   border-radius: 50%;
   padding-top: 5px;
-}
-input {
-  background: none;
-  border: none;
-  text-align: center;
 }
 .text {
   width: 500px;
@@ -130,5 +146,9 @@ img {
   display: flex;
   margin: auto;
   gap: 5px;
+}
+.i{
+font-size: 2em
+;
 }
 </style>
