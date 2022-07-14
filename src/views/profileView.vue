c19d5108f93239f4554f4117afd7b5f3a6ff4a67
unknown <aztoorabally7447@gmail.com>
2022-07-14 12:23:27 +0200

    update

--- src/views/profileView.vue
+++ src/views/profileView.vue
@@ -1,26 +1,134 @@
-<template lang="">
-    <div>
-        <h2>profile page</h2>
-    <div class="profileCard">
-<!-- user icon -->
-<img src="{{}}"
-<!-- has class for notications
-     check if messages[].length -->
-<!-- edit user icon button (small) -->
-<!-- user name -->
-<!-- user email -->
-<!-- user password -->
-<!-- user posts -->
-<!-- user friends list -->
-
+<template>
+  <section id="user-page">
+    <!-- user icon -->
+    <!-- has class for notications
+         check if messages[].length -->
+    <!-- edit user icon button (small) -->
+    <!-- user name -->
+    <!-- user email -->
+    <!-- user password -->
+    <!-- user posts -->
+    <!-- user friends list -->
+    <div class="user">
+      <div class="user_details container">
+        <h1>Profile</h1>
+        <div id="user-img"><img :src="imgUrl" alt="user" class="img" /></div>
+        <H1
+          ><input type="text" name="user" id="i" :placeholder="UserName"
+        /></H1>
+        <div class="bio">
+          <H2>Bio</H2>
+          <div class="text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Reiciendis, saepe totam. Fugit consequatur necessitatibus quis
+              cumque, nostrum et repellat aliquid nobis ducimus ullam
+              repudiandae possimus? Adipisci porro a quas dolorem!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="posts">
+      <div class="post">
+        <div class="use">
+          <img :src="post_sender_img" alt="" />
+          <h3>{{ post_sender }}</h3>
+        </div>
+        <p>{{ post_text }}</p>
+      </div>
+      <div class="post">
+        <div class="use">
+          <img :src="post_sender_img" alt="" />
+          <h3>{{ post_sender }}</h3>
+        </div>
+        <p>{{ post_text }}</p>
+      </div>
     </div>
+  </section>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png",
+      UserName: "Lihle",
+      post_text:
+        "Lorem ipsum dolor sit amet consectetur odit distinctio officia laboriosam aliquam at, expedita dolorem earum autem et molestiae necessitatibus architecto aliquid dolore tenetur, voluptatem hic.",
+      post_sender: "Username",
+      post_sender_img: "https://i.postimg.cc/nrxbv1nL/icon-1.png",
+    };
+  },
+};
 </script>
-<style lang="">
-    
+<style scoped>
+#user-page {
+  background-image: url("https://cdn.shopify.com/s/files/1/1294/9703/products/proud-new-mother-inuit-art-card.gif?v=1625188889");
+}
+h1,
+h2,
+h3 {
+  color: #f5f5dc;
+}
+#user-page {
+  height: fit-content;
+  background: linear-gradient(0.5turn, #dbdb71, #d8a23a);
+}
+.user {
+  padding: 10%;
+  display: flex;
+  justify-content: center;
+}
+.user-details {
+  position: relative;
+  left: -15px;
+}
+.img {
+  width: 200px;
+  border: 2px solid black;
+  border-radius: 50%;
+  padding-top: 5px;
+}
+input {
+  background: none;
+  border: none;
+  text-align: center;
+}
+.text {
+  width: 500px;
+  margin: auto;
+  height: fit-content;
+}
+.user_details {
+  width: fit-content;
+  border: 1px solid black;
+  border-radius: 10px;
+}
+.posts {
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+.post {
+  width: 250px;
+  height: 200px;
+  border: 2px double #8f7245;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-bottom: 5px;
+}
+img {
+  width: 30px;
+  height: 50;
+  box-shadow: 2px 1px;
+}
+.use {
+  display: flex;
+  margin: auto;
+  gap: 5px;
+}
 </style>
